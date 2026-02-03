9eccd9c7733ccc9004fb1b6ffc493e31dc5e318c
Tim Hoch <timhoch74@gmail.com>
2026-02-03 10:01:50 +0100

    Demo 1 Hello World

---
+++ src/main/java/main/D01_HelloWorld.java
@@ -0,0 +1,15 @@
+package main;
+
+public class D01_HelloWorld {
+
+	public static void main(String[] args) {
+		String text; //Deklaration
+		
+		text = "Hello World";
+		
+		/* Ausgabe */		
+		System.out.println(text);
+
+	}
+
+}
